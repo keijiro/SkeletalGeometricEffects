1cd45318376abff688de738dd7ce669b230a0581
Keijiro Takahashi <keijiro@gmail.com>
2018-07-31 15:49:00 +0900

    Change to use Humanoid

--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 
 public class Skeleton : MonoBehaviour
 {
-    #region Editable attributes
+    #region Editable variables
 
-    [SerializeField] Transform _root;
-    [SerializeField] float _minJointDistance = 0.12f;
-
-    [Space]
+    [SerializeField] Animator _animator;
     [SerializeField] Color _color = Color.white;
     [SerializeField] float _metallic = 0;
     [SerializeField] float _smoothness = 0.5f;
@@ -19,7 +16,30 @@ public class Skeleton : MonoBehaviour
 
     #region Private field memebers
 
-    List<Transform> _joints; // should be multiple of two
+    static readonly HumanBodyBones[] BonePairs = new [] {
+        HumanBodyBones.Hips,            HumanBodyBones.LeftUpperLeg,
+        HumanBodyBones.LeftUpperLeg,    HumanBodyBones.LeftLowerLeg,
+        HumanBodyBones.LeftLowerLeg,    HumanBodyBones.LeftFoot,
+        HumanBodyBones.LeftFoot,        HumanBodyBones.LeftToes,
+
+        HumanBodyBones.Hips,            HumanBodyBones.RightUpperLeg,
+        HumanBodyBones.RightUpperLeg,   HumanBodyBones.RightLowerLeg,
+        HumanBodyBones.RightLowerLeg,   HumanBodyBones.RightFoot,
+        HumanBodyBones.RightFoot,       HumanBodyBones.RightToes,
+
+        HumanBodyBones.Hips,            HumanBodyBones.Chest,
+        HumanBodyBones.Chest,           HumanBodyBones.Neck,
+        HumanBodyBones.Neck,            HumanBodyBones.Head,
+
+        HumanBodyBones.Neck,            HumanBodyBones.LeftUpperArm,
+        HumanBodyBones.LeftUpperArm,    HumanBodyBones.LeftLowerArm,
+        HumanBodyBones.LeftLowerArm,    HumanBodyBones.LeftHand,
+
+        HumanBodyBones.Neck,            HumanBodyBones.RightUpperArm,
+        HumanBodyBones.RightUpperArm,   HumanBodyBones.RightLowerArm,
+        HumanBodyBones.RightLowerArm,   HumanBodyBones.RightHand
+    };
+
     List<Vector3> _vertices;
     List<Vector3> _normals;
 
@@ -32,14 +52,13 @@ public class Skeleton : MonoBehaviour
 
     void Start()
     {
-        _joints = new List<Transform>();
-        EnumerateJointsRecursively(_root, _root);
+        var boneCount = BonePairs.Length;
 
-        _vertices = new List<Vector3>(_joints.Count);
-        _normals = new List<Vector3>(_joints.Count);
-        var indices = new int[_joints.Count];
+        _vertices = new List<Vector3>(boneCount);
+        _normals = new List<Vector3>(boneCount);
+        var indices = new int[boneCount];
 
-        for (var i = 0; i < _joints.Count; i++)
+        for (var i = 0; i < boneCount; i++)
         {
             _vertices.Add(Vector3.zero);
             _normals.Add(Vector3.up);
@@ -63,15 +82,15 @@ public class Skeleton : MonoBehaviour
 
     void Update()
     {
-        for (var i = 0; i < _joints.Count; i += 2)
+        for (var i = 0; i < BonePairs.Length; i += 2)
         {
-            var j1 = _joints[i];
-            var j2 = _joints[i + 1];
+            var bone1 = _animator.GetBoneTransform(BonePairs[i    ]);
+            var bone2 = _animator.GetBoneTransform(BonePairs[i + 1]);
 
-            _vertices[i] = j1.position;
-            _vertices[i + 1] = j2.position;
+            _vertices[i    ] = bone1.position;
+            _vertices[i + 1] = bone2.position;
 
-            _normals[i] = _normals[i + 1] = CalculateBoneNormal(j1, j2);
+            _normals[i] = _normals[i + 1] = bone1.up;
         }
 
         _mesh.SetVertices(_vertices);
@@ -85,42 +104,6 @@ public class Skeleton : MonoBehaviour
             _mesh, transform.localToWorldMatrix,
             _material, gameObject.layer
         );
-    }
-
-    #endregion
-
-    #region Local methods
-
-    void EnumerateJointsRecursively(Transform origin, Transform parent)
-    {
-        foreach (Transform node in parent)
-        {
-            var d = Vector3.Distance(origin.position, node.position);
-
-            if (d < _minJointDistance)
-            {
-                EnumerateJointsRecursively(origin, node);
-            }
-            else
-            {
-                _joints.Add(origin);
-                _joints.Add(node);
-                EnumerateJointsRecursively(node, node);
-            }
-        }
-    }
-
-    Vector3 CalculateBoneNormal(Transform joint1, Transform joint2)
-    {
-        return joint1.up;
-        /*
-        var v = joint2.position - joint1.position;
-        var dx = Mathf.Abs(Vector3.Dot(v, joint1.right));
-        var dy = Mathf.Abs(Vector3.Dot(v, joint1.up));
-        var dz = Mathf.Abs(Vector3.Dot(v, joint1.forward));
-        // Use local Y axis iff local X axis is aligned to the bone.
-        return (dx > dy && dx > dz) ? joint1.up : joint1.right;
-        */
     }
 
     #endregion
